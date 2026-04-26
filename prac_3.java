3aa19a4e3c8019ca75f473295f78834e0b1c5c71
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-04-26 13:32:28 +0530

    Add files via upload

---
+++ prac_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VowelCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a letter: ");
+        char ch = sc.next().charAt(0);
+
+        ch = Character.toLowerCase(ch);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("It is a vowel.");
+        } else {
+            System.out.println("It is a consonant.");
+        }
+    }
+}
