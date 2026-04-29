81df6d38857be27d251fb84ce7ee34a699d79e7c
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-04-29 18:04:24 +0530

    prac_19.java

--- prac_19.java
+++ prac_19.java
@@ -14,3 +14,31 @@ public class Student {
         System.out.println(name);
     }
 }
+
+package exam;
+
+import student.Student;
+
+public class Result extends Student {
+    int m1, m2, m3;
+
+    public Result(int rollNo, String name, int m1, int m2, int m3) {
+        super(rollNo, name);
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+    }
+
+    public void displayResult() {
+        displayStudent();
+        int total = m1 + m2 + m3;
+        double avg = total / 3.0;
+        System.out.println(total);
+        System.out.println(avg);
+    }
+
+    public static void main(String[] args) {
+        Result r = new Result(1, "Manthan", 70, 80, 90);
+        r.displayResult();
+    }
+}
