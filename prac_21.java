4c22c7fab8574430d1d621c668e146a9092b1ab9
Manthanpatel42 <manthanpatel0643@gmail.com>
2026-04-29 18:12:48 +0530

    Add files via upload

---
+++ prac_21.java
@@ -0,0 +1,19 @@
+class VotingApp {
+    void checkEligibility(int age) {
+        if (age < 18) throw new IllegalArgumentException("Age must be 18 or above to vote");
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        VotingApp v = new VotingApp();
+
+        try {
+            v.checkEligibility(16);
+            v.checkEligibility(20);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Validation process completed");
+        }
+    }
+}
